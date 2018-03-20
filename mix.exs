34cedc52d04d422208c4ed14ef175d86e60523bc
Ray Zane <ray@promptworks.com>
2018-03-20 14:05:40 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Authenticator.Mixfile do
   def project do
     [
       app: :authenticator,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
